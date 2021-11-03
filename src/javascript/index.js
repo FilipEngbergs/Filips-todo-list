a954b82cc4929ef8c8132c169e1102cb3031cb7e
Filipengbergs <filip792@hotmail.com>
2021-11-03 23:04:29 +0100

    Created a list and a button

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,29 @@
+class Lista {
+    constructor(Todo1, Todo2, Todo3, Todo4, Todo5) {
+        this.todo1 = Todo1;
+        this.todo2 = Todo2;
+        this.todo3 = Todo3;
+        this.todo4 = Todo4;
+        this.todo5 = Todo5;
+    }
+}
+
 window.onload = function () {
     //Selections
+    let firstList = new Lista(
+        "Fiska",
+        "Laga mat",
+        "Tvätta",
+        "Borsta tänderna",
+        "Gå promenad"
+    );
+    let makeListButton = document.createElement("button");
+    makeListButton.innerHTML = "Create List";
+    let myForm = document.querySelector("form");
     //EventListener
+    makeListButton.addEventListener("click", makeMyListFunction);
+    //Appendchild
+    myForm.appendChild(makeListButton);
 };
 //Functions
+function makeMyListFunction() {}
