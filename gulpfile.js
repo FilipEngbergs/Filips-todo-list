9dd08b5451ec0fc0d888b8ac416393c619166ace
Filipengbergs <filip792@hotmail.com>
2021-11-03 10:48:41 +0100

    commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass")(require("sass"));
+
+gulp.task("compile", function () {
+    return gulp
+        .src("src/scss/main.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"));
+});
+
+gulp.task("watch-scss", function () {
+    gulp.watch("src/scss/main.scss", gulp.series("compile"));
+});
+
+gulp.task("default", gulp.series("compile", "watch-scss"), function () {});
